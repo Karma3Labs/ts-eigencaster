75b8d36385285d88c8eba13f2b298e8fce57a687
Dimitris Lamprinos <pkakelas@gmail.com>
2022-12-19 20:49:16 +0200

    Add dockerfile.scraper

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,7 @@
+require('dotenv').config()
+
 // Update with your config settings.
+
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
@@ -6,6 +9,12 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgresql://localhost/farcaster' // process.env.PG_CONNECTION_STRING,
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    }
   }
 };
