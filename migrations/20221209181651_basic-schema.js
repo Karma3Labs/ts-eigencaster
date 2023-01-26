571d6a72fc1f64ec101f529e1cc29db98ff9dc8e
Dimitris Lamprinos <pkakelas@gmail.com>
2023-01-26 20:35:04 +0100

    Readd profile and follows migration

--- migrations/20221209181651_basic-schema.js
+++ migrations/20221209181651_basic-schema.js
@@ -3,7 +3,28 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-	return knex.schema.createTableIfNotExists('casts', function (table) {
+	return knex.schema.createTableIfNotExists('profiles', function (table) {
+		table.integer('fid').unique();
+		table.string('address').unique();
+		table.string('username');
+		table.string('display_name').nullable();
+		table.string('avatar_url', 1000).nullable();
+		table.boolean('avatar_verified');
+		table.string('followers');
+		table.string('following');
+		table.string('bio', 1000).nullable();
+		table.string('telegram').nullable();
+		table.string('referrer').nullable();
+		table.string('connected_address');
+		table.dateTime('registered_at');
+		table.datetime('updated_at');
+		table.json('custom_metrics');
+	}).createTableIfNotExists('follows', function (table) {
+		table.integer('follower_fid');
+		table.integer('following_fid');
+		table.dateTime('created_at');
+		table.unique(['follower_fid', 'following_fid'])
+	}).createTableIfNotExists('casts', function (table) {
 		table.string('hash');
 		table.string('thread_hash').nullable();
 		table.integer('author_fid');
@@ -27,5 +48,5 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-	return knex.schema.dropTable('casts')//.schema.dropTable('casts').dropTable('follows').dropTable('profiles');
+	return knex.schema.dropTable('casts').schema.dropTable('casts').dropTable('follows').dropTable('profiles');
 };
