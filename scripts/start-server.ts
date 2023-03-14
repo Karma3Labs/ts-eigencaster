a3379f9a5f48f719605aa3da3b840b447cfad56d
Dimitris Lamprinos <pkakelas@gmail.com>
2023-03-14 15:10:20 -0400

    Linter fix

--- scripts/start-server.ts
+++ scripts/start-server.ts
@@ -2,8 +2,7 @@ import Recommender from '../recommender'
 import serve from '../server/index'
 
 const main = async () => {
-	const recommender = new Recommender()
-	serve(recommender)
+	serve()
 }
 
 main()
