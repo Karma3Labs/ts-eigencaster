7aefc0269606d7896c2732bdb86e663350b5e2ef
Vijay Mariadassou <vijay@karma3labs.com>
2023-09-14 11:12:47 -0700

    reduce downtime during deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-sudo systemctl stop ts-eigencaster
-docker container rm ts-eigencaster
-docker image rm ts-eigencaster
+
+# build a new image
 docker build . -t ts-eigencaster
+
+# stop running container and delete it
+sudo systemctl stop ts-eigencaster
+
+# start container with new image
 sudo systemctl start ts-eigencaster
+
+# clean up old dangling images
+docker image prune -f
+
+# tail logs
 sudo journalctl -u ts-eigencaster -f -n20
